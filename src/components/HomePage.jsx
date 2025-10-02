b922b3c078720783bdc71d36d7e4c871ef16f130
yukimizu <yukidokemizu7189@gmail.com>
2025-10-02 05:22:32 +0000

    home prepare

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div class="container mb-auto p-4">
-      <div class="row">
-        <div>
-          <h1 class="text-center">home工事中</h1>
-        </div>        
+    <div>
+      <section className="text-gray-600 body-font">
+      <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+    <div className="text-center lg:w-2/3 w-full">
+     <img className="object-contain object-cover object-center rounded" src="public/logokari.jpg" alt='yukimizu_logo'></img>
+      <h1 className="title-font sm:text-4xl text-3xl mb-2 font-medium text-gray-900">yukimizu</h1>
+      <p className="mb-8 leading-relaxed">ゆきみず</p>
+      <div className="flex justify-center">
+        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
       </div>
+    </div>
+  </div>
+</section>
     </div>
   )
 }
