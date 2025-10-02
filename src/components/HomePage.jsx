055bcf6f5dc63432a900bfa3f5da7634966c3bc7
yukimizu <yukidokemizu7189@gmail.com>
2025-10-02 06:31:04 +0000

    fix bio

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -9,7 +9,7 @@ const HomePage = () => {
           <img className="object-contain object-center rounded w-48 h-auto" src="logologo.jpg" alt='yukimizu_logo'></img>
           <div className="text-center lg:w-2/3 w-full">
             <h1 className="title-font sm:text-4xl text-5xl mb-2 font-bold text-gray-900">yukimizu</h1>
-            <p className="mb-8 leading-relaxed">photograph / video / coding (newb) / cosplay</p>
+            <p className="mb-8 leading-relaxed">photo / video / coding (newb) / cosplay</p>
             <div className="flex justify-center">
               <a href="https://x.com/yukimizu7189" target="_blank" className="no-underline m-2 inline-flex text-white bg-blue-500 border-0 py-2 px-6 hover:bg-blue-600 rounded text-lg">Twitter</a>
               <Link to="/works" className="no-underline m-2 inline-flex text-white bg-emerald-400 border-0 py-2 px-6 hover:bg-emerald-600 rounded text-lg">Learn more!</Link>
